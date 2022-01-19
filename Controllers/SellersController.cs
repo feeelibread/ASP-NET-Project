31c2e4491929a7204a0f61af2c9ff294d59681cd
feeelibread <feeelpoos@gmail.com>
2022-01-19 12:28:52 -0300

    Validation

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -77,6 +77,13 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if(!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -96,6 +103,13 @@ namespace SalesWebMvc.Controllers
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
+            }
+
+            if(!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
             }
 
             try
--- Models/Seller.cs
+++ Models/Seller.cs
@@ -8,17 +8,25 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required!")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
 
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} required!")]
+        [EmailAddress(ErrorMessage = "Enter a valid Email")]
         public string Email { get; set; }
 
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "{0} required!")]
         public DateTime BirthDate { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}")]
+        [Required(ErrorMessage = "{0} required!")]
+        [Range(100.00, 50000.00, ErrorMessage = "{0} must be from {1} to {2}")]
         public double Salary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
